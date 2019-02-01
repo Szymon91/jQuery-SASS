78de6b447da7673d4692f3bf80b12c7049d7e4d6
Szymon91 <Szymon91@poczta.onet.pl>
2019-02-01 17:08:11 +0100

    Druga część

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 $('.section__button').hover(function () {
         $(this).css('background', 'linear-gradient(to right, #9b23ea 10%, #211991 100%');
         $(this).css('color', 'white');
+        $(this).css('border', 'none');
         $(this).css('cursor', 'pointer');
     },
     function () {
         $(this).css('background', 'white');
         $(this).css('color', '#9b23ea');
         $(this).css('cursor', 'auto');
+        $(this).css('border', 'double 2px transparent');
+        $(this).css('border-radius', '7px');
+        $(this).css('background-image', 'linear-gradient(white, white), linear-gradient(to right, #9b23ea 10%, #211991 100%)');
+        $(this).css('background-origin', 'border-box');
+        $(this).css('background-clip', 'content-box, border-box');
     });
 
 $('.row__image:first-child').hover(function () {
-        $(this).css('border', '3px solid transparent');
-        $(this).css('border-image', 'linear-gradient(to right, #9b23ea 10%, #211991 100%');
-        $(this).css('border-image-slice', '1');
+        $(this).css('border', 'double 3px transparent');
+        $(this).css('border-radius', '3%');
+        $(this).css('background-image', 'linear-gradient(white, white), linear-gradient(-45deg, #9b23ea, #00a7a7)');
+        $(this).css('background-origin', 'border-box');
+        $(this).css('background-clip', 'content-box, border-box');
     },
     function () {
         $(this).css('border', 'none');
     });
 
 $('.row__image:nth-of-type(2)').hover(function () {
-        $('.row__image:nth-of-type(2)  i').css('color', 'white');
+        $('.row__image:nth-of-type(2)  i').css('background', 'white');
+        $('.row__image:nth-of-type(2)  i').css('background-clip', 'text');
+        $('.row__image:nth-of-type(2)  i').css('-webkit-text-fill-color', 'transparent');
         $(this).css('background', 'linear-gradient(to right, #9b23ea 10%, #211991 100%');
         $(this).css('color', 'white');
     },
     function () {
-        $('.row__image:nth-of-type(2)  i').css('color', 'orange');
+        $('.row__image:nth-of-type(2)  i').css('background', 'linear-gradient(120deg, rgb(253, 220, 74), rgb(252, 110, 34))');
+        $('.row__image:nth-of-type(2)  i').css('background-clip', 'text');
+        $('.row__image:nth-of-type(2)  i').css('-webkit-text-fill-color', 'transparent');
         $(this).css('background', 'white');
         $(this).css('color', 'black');
     });
@@ -35,3 +47,12 @@ $('.row__image__div--third').hover(function () {
     function () {
         $(this).css('background', 'rgba(0, 0, 255, .5');
     });
+
+$(".row__button").click(function() {
+    $('.row__image:nth-of-type(3)').after($('.row__image:nth-of-type(1)'));
+    $('.row__image:nth-of-type(3)').after($('.row__image:nth-of-type(1)'));
+
+    $('.row__button:nth-of-type(2)').before($('.row__image:nth-of-type(3)'));
+    $('.row__button:nth-of-type(1)').after($('.row__image:nth-of-type(1)'));
+});
+
